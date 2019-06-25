45534cebf7bf66013f1493eb5a1aaf7920222201
Pathos14489 <shinigamiking14489@gmail.com>
2019-06-25 06:17:34 -0700

    Changed clearance from CREATOR to USER

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 exports.name = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}`
 exports.description = `Does a ping thing`
 exports.usage = `**{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}**`
-exports.clearance = `CREATOR`
+exports.clearance = `USER`
 exports.nsfw = false
 
 exports.run = async (client, message, args) => {
