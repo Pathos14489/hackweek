a11b2f96b67332fb3244cc0f0fb9a986017d6c00
Paweł <44851575+zneix@users.noreply.github.com>
2019-06-28 00:16:10 +0200

    modified ticket.js and added reaction-core menu

--- commands/ticket.js
+++ commands/ticket.js
@@ -7,7 +7,7 @@ exports.nsfw = false
 exports.run = (client, message) => {
     message.command(false,async () => {
         //checking if user is banned from sending tickets, simple
-        if (client.database.supportBanned.includes(message.author.id)) throw "You have been banned from sending support tickets!"
+        if (client.database.supportBanned.includes(message.author.id)) throw "You are banned from sending support tickets!"
         if (client.database.ticketCooldown.includes(message.author.id)) throw "You are sending support tickets too quickly!\nTry again later."
         //simple declarations
         var cooldown = 1800000;
@@ -17,42 +17,23 @@ exports.run = (client, message) => {
         //I tried using switch(tDesc.length) {case:}, but I didn't succeed
         if (tDesc.length < 8) throw `Your description is too short!`
         if (tDesc.length > 255) throw `Your description is too long!`
-        //ticket embed being sent to client.config.ticketChannel
-        var Tembed = {
-            color: parseInt("0xff6b00"),
-            author: {
-                name:`Support Ticket Request!`,
-                icon_url:message.author.avatarURL
-            },
-            title: `${message.author.tag} has sent a request!`,
-            description:`${tDesc}`,
-            timestamp:message.createdTimestamp,
-            footer: {
-                icon_url:message.author.avatarURL,
-                text:message.author.id
-            }
-        }
-        //used await here, so that confirmation won't be sent before this executes
-        await client.channels.get(client.config.ticketChannel).send({embed:Tembed})
-        .then(async msg => {
-            await msg.react("⭕")
-            await msg.react("🅰")
-            await msg.react("🅱")
-            await msg.react("❌")
-            await msg.react("✅")
-        })
+        //ticket reaction menu being sent to client.config.ticketChannel
+        const options = require(`../src/reaction-core/ticketMenu`)(message, tDesc)
+        let ticketMenu = new client.RC.Menu(options.embed, options.buttons)
+        client.RCHandler.addMenus(ticketMenu)
+        //await here, because without it ticket ID is not saved before setTImeout(); function
+        // also, I decided that active tickets could be mapped by Menu's message.id, but it's not the best option (at least for me) so we should change it later I guess
+        await client.channels.get(client.config.ticketChannel).sendMenu(ticketMenu).then(msg => client.database.activeTickets.push(msg.id));
         client.database.ticketCooldown.push(message.author.id)
-        // TO DO: Finish this line here
-        // client.database.activeTickets.push(/*the 'ID' of newly created ticket*/)
         client.update();
-        //optimize this thing here if you know how to >.<
+        //puts user in a cooldown array in database (optimize this thing here if you know how to >.<)
         setTimeout(function(){
             var index = client.database.ticketCooldown.indexOf(message.author.id);
             if (index > -1) {
                 client.database.ticketCooldown.splice(index, 1);
             }
             client.update();
-            console.log(`Removed cooldown of ${message.author.id}`)
+            console.log(`Removed ticket cooldown of ${message.author.id}`)
         },cooldown);
         //confirmation message for user, which tells that their ticket was sent successfully
         var Iembed = {
@@ -68,7 +49,6 @@ exports.run = (client, message) => {
             },
             timestamp:message.createdTimestamp
         }
-        //no message delete here on purpose (staff would see, who spammed tickets if that's the case)
-        message.channel.send({embed:Iembed})
+        message.channel.send({embed:Iembed}) //no message delete here on purpose (staff would see, who spammed tickets if that's the case)
     })
 }
