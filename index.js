f68e662ae456ea1d2d00ef5957f15c05e041ba03
Paweł <44851575+zneix@users.noreply.github.com>
2019-06-29 04:56:50 +0200

    modified client.login tokens

--- index.js
+++ index.js
@@ -1,15 +1,74 @@
-const discord = require('discord.js');
+const Discord = require('discord.js');
 const fs = require('fs');
 const enmap = require('enmap');
 const schedule = require('node-schedule');
-const client = new discord.Client();
-const {embed} = new discord.RichEmbed();
-const config = require(`./config.json`);
+const branch = require("git-branch")
+require('npm-package-to-env').config()
+require("./extensions/message")
+
+//BlueBird Long Stack Traces rock
+var Promise = require("bluebird")
+Promise.config({
+    longStackTraces: true
+})
+
+//TODO: Extension-ify the client extras to shrink index. (e.g. ./extensions/client.js)
+
+//Loads JSONs
+const database = require(`./src/json/database.json`);
+const config = require(`./src/json/config.json`);
+const tokens = require(`./src/json/tokens.json`);
+
+//Requires and objectifies client extras
+const client = new Discord.Client();
+client.RC = require("reaction-core")
 client.config = config;
-const database = require(`./database.json`);
+client.version = process.env.npm_package_version
 client.database = database;
-client.fs = fs;
 client.commands = new enmap();
+client.RCHandler = new client.RC.Handler
+
+//Defines Variables
+var update = 5 //How many minues between each time the update function is ran.
+
+//For Manually saving the config file
+client.saveConfig = async function(){
+    return new Promise((resolve,reject) => {
+        fs.writeFile(__dirname+"/src/json/config.json", JSON.stringify(client.config, null, 4), function(err) {
+            resolve("Saved!")
+            if(err) {
+                reject(err)
+            }
+        })
+    })
+}
+//For manually saving the DB
+client.saveDB = async function(){
+    return new Promise((resolve,reject) => {
+        fs.writeFile(__dirname+"/src/json/database.json", JSON.stringify(client.database, null, 4), function(err) {
+            resolve("Saved!")
+            if(err) {
+                reject(err)
+            }
+        })
+    })
+}
+
+//For Manually running the update function.
+client.update = async function(bool){
+    if(!bool) var bool = false
+    console.info(`${bool?"An Automated":`A Manual`} Update Ran @ ${new Date()}`)
+    client.saveConfig()
+    client.saveDB()
+}
+
+//Runs update every ${update} minutes
+//To clarify, this is not to be relied on, only if you forget to manually update.
+schedule.scheduleJob(`*/${update} * * * *`, () => {
+    client.update(true)
+})
+
+//Loads the events
 fs.readdir('./events', (err, files) => {
     if (err) return console.error(err);
     files.forEach(file => {
@@ -19,6 +78,7 @@ fs.readdir('./events', (err, files) => {
         delete require.cache[require.resolve(`./events/${file}`)];
     });
 });
+//Loads all commands currently in the ./commands directory.
 fs.readdir(`./commands`, (err, files) => {
     if (err) return console.error(err);
     files.forEach(file => {
@@ -28,5 +88,6 @@ fs.readdir(`./commands`, (err, files) => {
         client.commands.set(name, props);
     });
 });
-const {token} = require('./auth.json');
-client.login(token);
+
+//Logs token into Discord API based on which branch is the working branch at the moment
+client.login(tokens.dev)
